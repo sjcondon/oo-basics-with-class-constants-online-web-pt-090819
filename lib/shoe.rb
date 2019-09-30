6504eb777d75f5920505f7abcb8e5500b44c78d5
sarah <s.j.condon01@gmail.com>
2019-09-30 03:15:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,34 @@
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+    #or GENRES = @genre
+  end
+
+end
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
+
   BRANDS = []
 
   def initialize(brand)
     @brand = brand
-  end
-  
-  if !(BRANDS.include?(@brand))
+    if !(BRANDS.include?(@brand))
       BRANDS << @brand
     end
   end
@@ -18,4 +38,6 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
 end
+
