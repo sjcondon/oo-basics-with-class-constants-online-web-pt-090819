4b6c506261c477e84ae7501f09762c707fcf26c0
sarah <s.j.condon01@gmail.com>
2019-09-30 03:16:04 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,38 @@
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+    #or GENRES = @genre
+  end
+
+end
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
+    end
   end
+
 
   def cobble
     self.condition = "new"
@@ -12,3 +40,4 @@ class Shoe
   end
 
 end
+
